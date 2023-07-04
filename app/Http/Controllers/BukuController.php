39ad9540de3346913743009df4cba200672e6d62
zaelanirizal02 <zaelanirizal02@gmail.com>
2023-07-04 23:06:41 +0700

    CRUD API

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -15,7 +15,10 @@ class BukuController extends Controller
         $client = new Client();
         $url = 'http://localhost:8000/api/buku';
         $response = $client->request('GET', $url);
-        dd($response);
+        $content = $response->getBody()->getContents();
+        $contentArray = json_decode($content,true);
+        $data = $contentArray['data'];
+        return view ('buku.index',['data'=>$data]);
     }
 
     /**
@@ -30,8 +33,34 @@ class BukuController extends Controller
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        //
+    {   
+        $judul  = $request->judul;
+        $pengarang = $request->pengarang;
+        $tanggal_publikasi = $request->tanggal_publikasi;
+
+        $parameter = [
+            'judul'=>$judul,
+            'pengarang'=>$pengarang,
+            'tanggal_publikasi'=>$tanggal_publikasi,
+        ];
+
+        $client = new Client();
+        $url = 'http://localhost:8000/api/buku';
+        $response = $client->request('POST', $url, [
+            'headers' => ['Content-type' => 'application/json'],
+            'body' =>  json_encode($parameter)
+        ]);
+        $content = $response->getBody()->getContents();
+        $contentArray = json_decode($content,true);
+        // $data = $contentArray['data'];
+        // print_r($data);
+       if($contentArray['status'] != true){
+        $error = $contentArray['data'];
+        return redirect()->to('buku')->withErrors($error)->withInput();
+       }
+       else{
+        return redirect()->to('buku')->with('success', 'berhasil update data');
+       }
     }
 
     /**
@@ -47,7 +76,18 @@ class BukuController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $client = new Client();
+        $url = "http://localhost:8000/api/buku/$id";
+        $response = $client->request('GET', $url);
+        $content = $response->getBody()->getContents();
+        $contentArray = json_decode($content,true);
+        if($contentArray['status'] != true){
+            $error = $contentArray['message'];
+            return redirect()->to('buku')->withErrors($error);
+        }else{
+            $data = $contentArray['data'];
+            return view('buku.index',['data'=>$data]);
+        }
     }
 
     /**
@@ -55,7 +95,33 @@ class BukuController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $judul  = $request->judul;
+        $pengarang = $request->pengarang;
+        $tanggal_publikasi = $request->tanggal_publikasi;
+
+        $parameter = [
+            'judul'=>$judul,
+            'pengarang'=>$pengarang,
+            'tanggal_publikasi'=>$tanggal_publikasi,
+        ];
+
+        $client = new Client();
+        $url = "http://localhost:8000/api/buku/$id";
+        $response = $client->request('PUT', $url, [
+            'headers' => ['Content-type' => 'application/json'],
+            'body' =>  json_encode($parameter)
+        ]);
+        $content = $response->getBody()->getContents();
+        $contentArray = json_decode($content,true);
+        // $data = $contentArray['data'];
+        // print_r($data);
+       if($contentArray['status'] != true){
+        $error = $contentArray['data'];
+        return redirect()->to('buku')->withErrors($error)->withInput();
+       }
+       else{
+        return redirect()->to('buku')->with('success', 'berhasil memasukan data');
+       }
     }
 
     /**
@@ -63,6 +129,19 @@ class BukuController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $client = new Client();
+        $url = "http://localhost:8000/api/buku/$id";
+        $response = $client->request('DELETE', $url); 
+        $content = $response->getBody()->getContents();
+        $contentArray = json_decode($content,true);
+        // $data = $contentArray['data'];
+        // print_r($data);
+       if($contentArray['status'] != true){
+        $error = $contentArray['data'];
+        return redirect()->to('buku')->withErrors($error)->withInput();
+       }
+       else{
+        return redirect()->to('buku')->with('success', 'berhasil menghapus data');
+       }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::resource('/posts', \App\Http\Controllers\PostController::class);
 
 Route::get('buku',[BukuController::class, 'index']);
+Route::post('buku',[BukuController::class, 'store']);
+Route::get('buku/{id}',[BukuController::class, 'edit']);
+Route::put('buku/{id}',[BukuController::class, 'update']);
+Route::delete('buku/{id}',[BukuController::class, 'destroy']);
