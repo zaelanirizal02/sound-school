e13fdc2fbd9ee3ca12fcaae349c90d07c3c365d7
zaelanirizal02 <zaelanirizal02@gmail.com>
2023-07-04 14:32:26 +0700

    error api

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BukuController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource('/posts', \App\Http\Controllers\PostController::class);
+
+Route::get('buku',[BukuController::class, 'index']);
