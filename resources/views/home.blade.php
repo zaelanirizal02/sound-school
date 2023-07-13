913acc1c53805ed33e59a6dee624903b912e10ee
zaelanirizal02 <zaelanirizal02@gmail.com>
2023-07-13 22:31:49 +0700

    home2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,7 +23,7 @@
           <a class="nav-link" href="#">Features</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="#">Pricing2</a>
         </li>
         <li class="nav-item">
           <a class="nav-link disabled">Disabled</a>
